b647f6f0dfe45f31e347531a99212372187ff5bd
BANCEL Thomas <thomas.bancel@icm-institute.org>
2018-05-15 17:17:38 +0200

    Closed loop ready. Next steps: work on smaller sampling (50ms) and playback on very large amount of files.

--- closed_loop_scripts/old/stimulate_line_length2.m
+++ closed_loop_scripts/old/stimulate_line_length2.m
@@ -1,17 +1,6 @@
-% script real time CED (almost real time)
-clc; close all;
-clear;
+% script which is called from closed loop
 
-%%%%
-% GENERAL CONFIG
-% change parameters here to adjust the sampling rate, epoch_length etc.
-approx_epoch_timelength = 0.2; % 200ms
-recording_time = 1*60; % in seconds
-sampling_rate_ced = 1000; % in Hz /!\ be careful, period must be a multiple of the clock period.
-channel_to_sample = 4;
-% END OF GENERAL CONFIG
-%%%%
-
+%%% SCRIPT RUNNING
 fs = sampling_rate_ced;
 dt = 1/fs;
 n_points_epoch = fs*approx_epoch_timelength;
@@ -48,7 +37,6 @@ if ~(Master8.Connect)
 end;
 % Change to paradigm7
 Master8.ChangeParadigm(7);
-% Master8.SetChannelDuration(3, stimulation_duration);
 % END OF MASTER 8 CONNEXION
 %%%%%%
 
@@ -67,7 +55,11 @@ res=0;
 % END OF CED CONNECTION
 %%%%%%
 
+y_scale = 5/(2^(n_bytes*8)/2);
+stimulation_times = [];
 epoch_starts = [];
+t = datetime('now');
+timestr = strcat(num2str(yyyymmdd(t)), '_', num2str(t.Hour), '_', num2str(t.Minute));
 
 % Launch the sampling
 for i=1:number_of_loops
@@ -77,29 +69,45 @@ for i=1:number_of_loops
     % Refer to Programmw manual
     % matced64c('cedSendString','ADCBST,kind,byte,start_in_memory,n_bytes_array,chan,n_cycles,clock,pre,count;');
     
+    % send TTL to CED Recording (Severine) to notify that sampling Starts
     if i == 1
-        tic
-        % send TTL to CED Recording (Severine) to notify that sampling Starts
+        tic    
         Master8.Trigger(6);
     end
     
     epoch_starts(i,1) = toc;
-    matced64c('cedSendString',ced_cmd_str);
+    matced64c('cedSendString',ced_cmd_str); % really starts sampling here
+    
+    % compute calculation
+    if i>1
+        line_length(i-1,1) = feature_line_length(data(i-1,:));
+        
+        if line_length(i-1,1) / norm_baseline > threshold_value_nf_ll;
+           seizures(i-1,1) = 1;
+           Master8.Trigger(3);
+           stimulation_times = [stimulation_times, toc];
+        else
+            seizures(i-1,1)=0;
+        end
+        
+        % analyse the line length in 1
+        time_to_compute(i,1) = toc - time_elapsed(i-1,1);
+    end
     
     % Ask the CED if it has finished sampling
     res=-1;
     while res ~=0
        matced64c('cedSendString','ADCBST,?;');
        res=eval(matced64c('cedGetString'));
-
-       % disp(strcat("Status of sampling: ", int2str(res)));
-       % drawnow; % flushes the event queue
     end
     
     % As soon as sampling has ended, get the data, no. of words not bytes
-    % Save it into the RAM of the computer, record the time, display sth and get to next
+    % Save it into the RAM, record the time, display sth and get to next
     % sampling
-    data(i,:)=matced64c('cedToHost', n_points_epoch, 0);
+    data(i,:)=matced64c('cedToHost', n_points_epoch, 0)*y_scale;
+    
+    % fdata(i,:) = filtfilt(b, a, data(i,:));
+    
     time_elapsed(i,1) = toc; %at the end of the epochs
     disp(strcat("Time elapsed: ", num2str(time_elapsed(i,1))));
     drawnow;
@@ -111,13 +119,6 @@ for i=1:number_of_loops
     
 end
 
-% res=matced64c('cedCloseX');
-% extract the channel data
-
-% rescale measured values
-y_scale = 5/(2^(n_bytes*8)/2);
-data = data*y_scale;
-
 % reconstruct time thanks to time_elapsed
 ideal_time = ones(number_of_loops, n_points_epoch).*(1:n_points_epoch)/fs;
 delay = circshift(ones(number_of_loops, n_points_epoch).*time_elapsed, 1, 1);
@@ -127,15 +128,36 @@ real_time = ideal_time + delay;
 realtime = reshape(real_time', [1 numel(real_time)]);
 realdata = reshape(data', [1 numel(data)]);
 
-% plot the recorded period
-figure(1)
+stim_vector = zeros(1,size(realtime, 2));
+for i=1:size(stimulation_times, 2)
+    start_stim_index = min(find(realtime > stimulation_times(1,i)));
+    end_stim_index = max(find(realtime < stimulation_times(1,i)+ stimulation_duration));
+    stim_vector(1,start_stim_index:end_stim_index)=1;
+end
+
+% plot the stimulation and the realdata:
+f2=figure(2);
+f2.Name = strcat(timestr,'_stimulation_line_length_recording_figure');
+h1=subplot(2,1,1)
 plot(realtime, realdata);
 xlabel("Time (s)");
 title(strcat("Channel CED Analysis: ", num2str(chan)));
+h2=subplot(2,1,2)
+plot(realtime, stim_vector);
+ylim([-1 2])
+xlabel("Time (s)");
+title("Stimulation");
+linkaxes([h1 h2], 'x');
+saveas(f2,strcat(f2.Name, '.png'), 'png');
 
-%%%%
-% Compute baseline :
-
-% input beginning of baseline (date) 's':
-
-% input end of baseline of baseline (date) 's':
+% Save the recorded data, features, stim_times, delay etc.
+str_filename = strcat(timestr, '_stimulation_line_length_recording.mat');
+s.filename = str_filename;
+s.realtime = realtime;
+s.realdata = realdata;
+s.sampled_data = data;
+s.time_elapsed = time_elapsed;
+s.mean_baseline_ll = 
+s.sampling_rate_ced = sampling_rate_ced;
+s.approx_epoch_timelength = approx_epoch_timelength;
+s.channel_to_sample = channel_to_sample;
